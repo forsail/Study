e506c5dc8d8dc05a3621634b0ed16ec8354c8c31
steveyan <tinggengyan@gmail.com>
2016-12-11 20:42:29 +0800

    [CrashHandler ] update CrashHandler

--- mylib/src/main/java/steve/cn/mylib/base/CrashHandler.java
+++ mylib/src/main/java/steve/cn/mylib/base/CrashHandler.java
@@ -1,104 +1,140 @@
 package steve.cn.mylib.base;
 
-import android.content.Context;
-import android.os.Looper;
-import android.util.Log;
-import android.widget.Toast;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Process;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.Thread.UncaughtExceptionHandler;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 /**
  * Created by yantinggeng on 2016/7/4.
  */
-public class CrashHandler implements Thread.UncaughtExceptionHandler {
+public class CrashHandler implements UncaughtExceptionHandler {
 
-    private static final String TAG = CrashHandler.class.getSimpleName();
-    private static CrashHandler instance; // 单例模式
-    private Context context; // 程序Context对象
-    private Thread.UncaughtExceptionHandler defalutHandler; // 系统默认的UncaughtException处理类
+    private static final String TAG = "CrashHandler";
+    private static final boolean DEBUG = true;
+
+    private static final String PATH = Environment.getExternalStorageDirectory().getPath() + "/Crash/log/";
+    private static final String FILE_NAME = "crash";
+    private static final String FILE_NAME_SUFFIX = ".trace";
+
+    private static CrashHandler sInstance = new CrashHandler();
+    private UncaughtExceptionHandler mDefaultCrashHandler;
+    private Context mContext;
     private DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss.SSS", Locale.CHINA);
+
 
     private CrashHandler() {
     }
 
-    /**
-     * 获取CrashHandler实例
-     *
-     * @return CrashHandler
-     */
     public static CrashHandler getInstance() {
-        if (instance == null) {
-            synchronized (CrashHandler.class) {
-                if (instance == null) {
-                    instance = new CrashHandler();
-                }
-            }
-        }
-
-        return instance;
+        return sInstance;
     }
 
-    /**
-     * 异常处理初始化
-     */
     public void init(Context context) {
-        this.context = context;
-        // 获取系统默认的UncaughtException处理器
-        defalutHandler = Thread.getDefaultUncaughtExceptionHandler();
-        // 设置该CrashHandler为程序的默认处理器
+        mDefaultCrashHandler = Thread.getDefaultUncaughtExceptionHandler();
         Thread.setDefaultUncaughtExceptionHandler(this);
+        mContext = context.getApplicationContext();
     }
 
     /**
-     * 当UncaughtException发生时会转入该函数来处理
+     * 这个是最关键的函数，当程序中有未被捕获的异常，系统将会自动调用#uncaughtException方法 thread为出现未捕获异常的线程，ex为未捕获的异常，有了这个ex，我们就可以得到异常信息。
      */
     @Override
     public void uncaughtException(Thread thread, Throwable ex) {
-        // 自定义错误处理
-        boolean res = handleException(ex);
-        if (!res && defalutHandler != null) {
-            // 如果用户没有处理则让系统默认的异常处理器来处理
-            defalutHandler.uncaughtException(thread, ex);
+        try {
+            //导出异常信息到SD卡中
+            dumpExceptionToSDCard(ex);
+            uploadExceptionToServer();
+            //这里可以通过网络上传异常信息到服务器，便于开发人员分析日志从而解决bug
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        ex.printStackTrace();
+
+        //如果系统提供了默认的异常处理器，则交给系统去结束我们的程序，否则就由我们自己结束自己
+        if (mDefaultCrashHandler != null) {
+            mDefaultCrashHandler.uncaughtException(thread, ex);
         } else {
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                Log.e(TAG, "error : ", e);
+            Process.killProcess(Process.myPid());
+        }
+
+    }
+
+    private void dumpExceptionToSDCard(Throwable ex) throws IOException {
+        //如果SD卡不存在或无法使用，则无法把异常信息写入SD卡
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            if (DEBUG) {
+                Log.w(TAG, "sdcard unmounted,skip dump exception");
+                return;
             }
-            // 退出程序
-            android.os.Process.killProcess(android.os.Process.myPid());
-            System.exit(1);
+        }
+
+        File dir = new File(PATH);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        long current = System.currentTimeMillis();
+        String time = formatter.format(new Date(current));
+        File file = new File(PATH + FILE_NAME + time + FILE_NAME_SUFFIX);
+
+        try {
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+            pw.println(time);
+            dumpPhoneInfo(pw);
+            pw.println();
+            ex.printStackTrace(pw);
+            pw.close();
+        } catch (Exception e) {
+            Log.e(TAG, "dump crash info failed");
         }
     }
 
-    /**
-     * 自定义错误处理,收集错误信息 发送错误报告等操作均在此完成.
-     *
-     * @return true:如果处理了该异常信息;否则返回false.
-     */
-    private boolean handleException(final Throwable ex) {
-        if (ex == null) {
-            return false;
-        }
-        new Thread() {
-            @Override
-            public void run() {
-                Looper.prepare();
+    private void dumpPhoneInfo(PrintWriter pw) throws NameNotFoundException {
+        PackageManager pm = mContext.getPackageManager();
+        PackageInfo pi = pm.getPackageInfo(mContext.getPackageName(), PackageManager.GET_ACTIVITIES);
+        pw.print("App Version: ");
+        pw.print(pi.versionName);
+        pw.print('_');
+        pw.println(pi.versionCode);
 
-                ex.printStackTrace();
-                String err = "[" + ex.getMessage() + "]";
-                Toast.makeText(context, "程序出现异常." + err, Toast.LENGTH_LONG)
-                    .show();
+        //android版本号
+        pw.print("OS Version: ");
+        pw.print(Build.VERSION.RELEASE);
+        pw.print("_");
+        pw.println(Build.VERSION.SDK_INT);
 
-                Looper.loop();
-            }
+        //手机制造商
+        pw.print("Vendor: ");
+        pw.println(Build.MANUFACTURER);
 
-        }.start();
-        // TODO 保存日志文件
+        //手机型号
+        pw.print("Model: ");
+        pw.println(Build.MODEL);
 
-        return true;
+        //cpu架构
+        pw.print("CPU ABI: ");
+        pw.println(Build.CPU_ABI);
     }
+
+    private void uploadExceptionToServer() {
+        //TODO Upload Exception Message To Your Web Server
+    }
+
 }
