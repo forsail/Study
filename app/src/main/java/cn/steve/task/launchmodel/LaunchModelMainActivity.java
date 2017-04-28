1b259a5832be33f0f7a494d1071db18fc3ab03a1
yantinggeng <tinggengyan@gmail.com>
2017-04-28 13:57:14 +0800

    [launcher] launcher model and task flag

---
+++ app/src/main/java/cn/steve/task/launchmodel/LaunchModelMainActivity.java
@@ -0,0 +1,39 @@
+package cn.steve.task.launchmodel;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import cn.steve.study.R;
+
+/**
+ * 研究 启动模式
+ *
+ * Created by Steve on 2017/3/21.
+ */
+
+public class LaunchModelMainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_button);
+        button = (Button) findViewById(R.id.buttonMain);
+        textView = (TextView) findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LaunchModelMainActivity.this, SingleTaskActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/cn/steve/task/launchmodel/SingleInstanceActivity.java
@@ -0,0 +1,28 @@
+package cn.steve.task.launchmodel;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import cn.steve.study.R;
+
+/**
+ * Created by Steve on 2017/3/21.
+ */
+
+public class SingleInstanceActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_button);
+        button = (Button) findViewById(R.id.buttonMain);
+        textView = (TextView) findViewById(R.id.textView);
+
+    }
+}
