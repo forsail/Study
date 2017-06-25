42123db386acb7b5e3217b215a26f59b59a41d60
steveyan <tinggengyan@gmail.com>
2017-06-25 18:58:33 +0800

    [  coordinator ]  通过获取child的高度跳转
    准备通过获取child的高度来进行跳转

--- app/src/main/java/cn/steve/coordinatorlayout/CoordinatorLayoutActivity.java
+++ app/src/main/java/cn/steve/coordinatorlayout/CoordinatorLayoutActivity.java
@@ -5,10 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
 
+import cn.steve.recyclerview.SimpleRecyclerAdapter;
 import cn.steve.study.R;
 import cn.steve.viewpager.ViewPagerAdapter;
 
@@ -21,6 +25,22 @@ public class CoordinatorLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordinatorlayout);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.coordinatorLayoutRecyclerView);
+
+        //创建默认的线性LayoutManager ;设置成横向的,默认为竖屏的
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(mLayoutManager);
+        SimpleRecyclerAdapter mAdapter = new SimpleRecyclerAdapter(getDummyDatas());
+        mAdapter.setOnItemClickListener(new SimpleRecyclerAdapter.OnRecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View view, String data) {
+                int height = view.getHeight();
+                Log.i("height:", "" + height);
+            }
+        });
+        recyclerView.setAdapter(mAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -55,5 +75,13 @@ public class CoordinatorLayoutActivity extends AppCompatActivity {
             });
         }
 
+    }
+
+    private String[] getDummyDatas() {
+        String[] datas = new String[200];
+        for (int i = 0; i < datas.length; i++) {
+            datas[i] = "Data" + i;
+        }
+        return datas;
     }
 }
