270fab88d87ddefed8575357025001efcdc3a19c
yantinggeng <tinggengyan@gmail.com>
2017-07-14 16:45:21 +0800

    [ webview  ]  webview调起相机

--- app/src/main/java/cn/steve/webview/simple/WebMainActivity.java
+++ app/src/main/java/cn/steve/webview/simple/WebMainActivity.java
@@ -1,10 +1,26 @@
 package cn.steve.webview.simple;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import cn.steve.study.R;
 
@@ -14,15 +30,169 @@ import cn.steve.study.R;
 
 public class WebMainActivity extends Activity {
 
+    private final static int FILECHOOSER_RESULTCODE = 0x00012;
+    private ValueCallback<Uri> mUploadMessage;
+    private ValueCallback<Uri[]> mFilePathCallbackArray;
+    private boolean onShowFileChooser;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_button);
-        findViewById(R.id.buttonMain).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(WebMainActivity.this, WebViewActivity.class));
-            }
-        });
+        setContentView(R.layout.activity_webview);
+        WebView mWebView = (WebView) findViewById(R.id.webView1);
+        String url = "file:///android_asset/inputFileTest.html";
+        mWebView.loadUrl(url);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new MainWebChromeClient());
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == FILECHOOSER_RESULTCODE) {
+            if (!onShowFileChooser) {
+                if (null == mUploadMessage) {
+                    return;
+                }
+                Uri result = data == null ? null : data.getData();
+                mUploadMessage.onReceiveValue(result);
+                mUploadMessage = null;
+            } else {
+                // check that the response is a good one
+                if (null == mFilePathCallbackArray) {
+                    return;
+                }
+                onActivityResultAboveL(requestCode, resultCode, data);
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void onActivityResultAboveL(int requestCode, int resultCode, Intent data) {
+        if (requestCode != FILECHOOSER_RESULTCODE || mFilePathCallbackArray == null) {
+            return;
+        }
+        Uri[] results = null;
+        if (resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                String dataString = data.getDataString();
+                ClipData clipData = data.getClipData();
+                if (clipData != null) {
+                    results = new Uri[clipData.getItemCount()];
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
+                        ClipData.Item item = clipData.getItemAt(i);
+                        results[i] = item.getUri();
+                    }
+                }
+                if (dataString != null) {
+                    results = new Uri[]{Uri.parse(dataString)};
+                }
+            }
+        }
+        mFilePathCallbackArray.onReceiveValue(results);
+        mFilePathCallbackArray = null;
+    }
+
+
+    private class MainWebChromeClient extends WebChromeClient {
+
+        private Uri imageUri;
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+        }
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+        }
+
+        @SuppressWarnings("unused")
+        public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+            openFileChooserImpl(uploadMsg, "image/*");
+        }
+
+        @SuppressWarnings("unused")
+        // For Android 3.0+
+        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
+            openFileChooserImpl(uploadMsg, "*/*");
+        }
+
+        @SuppressWarnings("unused")
+        //For Android 4.1
+        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+            openFileChooserImpl(uploadMsg, "image/*");
+        }
+
+        //Android 5.0+
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+            onShowFileChooser = true;
+            mFilePathCallbackArray = filePathCallback;
+
+            File imageStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyApp");
+            // Create the storage directory if it does not exist
+            if (!imageStorageDir.exists()) {
+                imageStorageDir.mkdirs();
+            }
+            File file = new File(imageStorageDir + File.separator + "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+            imageUri = Uri.fromFile(file);
+
+            final List<Intent> cameraIntents = new ArrayList<Intent>();
+            final Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            final PackageManager packageManager = getPackageManager();
+            final List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
+            for (ResolveInfo res : listCam) {
+                final String packageName = res.activityInfo.packageName;
+                final Intent i = new Intent(captureIntent);
+                i.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+                i.setPackage(packageName);
+                i.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                cameraIntents.add(i);
+            }
+
+
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("image/*");
+            Intent chooserIntent = Intent.createChooser(i, "Image Chooser");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray(new Parcelable[]{}));
+
+            startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
+            return true;
+        }
+
+        private void openFileChooserImpl(ValueCallback<Uri> uploadMsg, String typeStr) {
+            File imageStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyApp");
+            // Create the storage directory if it does not exist
+            if (!imageStorageDir.exists()) {
+                imageStorageDir.mkdirs();
+            }
+            File file = new File(imageStorageDir + File.separator + "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+            imageUri = Uri.fromFile(file);
+
+            final List<Intent> cameraIntents = new ArrayList<Intent>();
+            final Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            final PackageManager packageManager = getPackageManager();
+            final List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
+            for (ResolveInfo res : listCam) {
+                final String packageName = res.activityInfo.packageName;
+                final Intent i = new Intent(captureIntent);
+                i.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+                i.setPackage(packageName);
+                i.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                cameraIntents.add(i);
+            }
+
+            mUploadMessage = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("image/*");
+            Intent chooserIntent = Intent.createChooser(i, "Image Chooser");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray(new Parcelable[]{}));
+            startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
+        }
+    }
+
 }
